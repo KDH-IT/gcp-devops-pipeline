1bdc4e15d5448a123a8326a92d24473575b3afa5
Jeonhyunjun <jeonhj920@gmail.com>
2022-02-13 16:47:13 +0900

    api 2

--- src/main/java/org/toyproject/devops/controller/SampleController.java
+++ src/main/java/org/toyproject/devops/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.toyproject.devops.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -8,6 +9,11 @@ import reactor.core.publisher.Mono;
 public class SampleController {
     @GetMapping("/")
     public Mono<String> hello(){
+        return Mono.just("hello");
+    }
+
+    @PostMapping("/")
+    public Mono<String> hello2(){
         return Mono.just("hello");
     }
 }
